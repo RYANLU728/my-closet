0e2bb7a5c7efe67edd4f1df18d9824a40b5caa32
RYANLU728 <163879263+RYANLU728@users.noreply.github.com>
2026-02-06 23:32:01 +0800

    Create items.ts

---
+++ public/src/data/items.ts
@@ -0,0 +1,12 @@
+export const CLOSET_DATA = [
+  { id: 1, name: "拉鏈布勞森外套", uq: "481606", color: "咖啡", style: "工裝" },
+  { id: 2, name: "工裝外套", uq: "483819", color: "墨綠", style: "工裝" },
+  { id: 3, name: "灰色大衣", uq: "N/A", color: "灰色", style: "大人感" },
+  { id: 4, name: "黑色大衣", uq: "N/A", color: "黑色", style: "大人感" },
+  { id: 5, name: "白踢", uq: "N/A", color: "白色", style: "基礎" },
+  { id: 6, name: "黑踢", uq: "N/A", color: "黑色", style: "基礎" },
+  { id: 7, name: "Polo領大學踢", uq: "N/A", color: "紅白灰", style: "復古" },
+  { id: 8, name: "藍色錐形牛仔褲", uq: "479235", color: "藍色", style: "City Boy" },
+  { id: 9, name: "白色寬鬆直筒褲", uq: "481436", color: "白色", style: "City Boy" },
+  { id: 10, name: "黑色寬牛仔褲", uq: "N/A", color: "黑色", style: "工裝" }
+];
